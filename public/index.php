b21b484ef706c774b13234a9ca509e1300ac795a
김진욱 <bachmani.sylvilagus@gmail.com>
2026-06-14 23:39:27 +0900

    Clarify parent menu validation message

--- public/index.php
+++ public/index.php
@@ -444,7 +444,7 @@ if ($path === '/admin/menus/update' && $method === 'POST') {
         } elseif ($parentId === $id) {
             $errors[] = '자기 자신을 상위 메뉴로 지정할 수 없습니다.';
         } elseif (Menu::findBySite($parentId, $siteId) === null) {
-            $errors[] = '상위 메뉴는 현재 선택한 사이트에 속한 메뉴만 입력할 수 있습니다.';
+            $errors[] = '상위 메뉴는 현재 선택한 사이트에 속한 메뉴만 선택할 수 있습니다.';
         }
     }
 
@@ -610,7 +610,7 @@ if ($path === '/admin/menus' && $method === 'POST') {
         if ($parentId <= 0) {
             $errors[] = '상위 메뉴 선택값이 올바르지 않습니다.';
         } elseif (Menu::findBySite($parentId, $siteId) === null) {
-            $errors[] = '상위 메뉴는 현재 선택한 사이트에 속한 메뉴만 입력할 수 있습니다.';
+            $errors[] = '상위 메뉴는 현재 선택한 사이트에 속한 메뉴만 선택할 수 있습니다.';
         }
     }
 
